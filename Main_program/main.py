0f59de2890ac73094035d1ea4f2b23dde307e60b
DusanB98 <dusanbobek98@gmail.com>
2025-09-08 15:16:36 +0200

    Fourth commit

--- Main_program/main.py
+++ Main_program/main.py
@@ -3,8 +3,11 @@
 
 import subprocess
 import platform
+import sqlite3
+import datetime
 
 file_path = "/home/dusan/Desktop/Github/Python_projects/Network_automation/Main_program/hosts.txt"
+data_log_file = "connection_hosts_log.db"
 
 # Reading file of hosts with error notification
 print()
@@ -49,10 +52,69 @@ def pinging_hosts_latency(host):
     except subprocess.CalledProcessError:
         return False                           # host is unavailable
 
-# Printing result of pinging
-for host in hosts:
-    latency = pinging_hosts_latency(host)
-    if pinging_hosts_latency(host):
-        print(f"✅ Host '{host}' is available, latency {latency:.2f} ms")
-    else:
-        print(f"❌ Host '{host}' is unavailable")
+#sqlite3 creating database
+def data_db_init():
+
+    # creating connection with database (if doesn't exist, it will create new)
+    database = sqlite3.connect(data_log_file)
+    
+    # creating SQL cursor, cmds are executed through this cursor
+    cursor = database.cursor()
+
+    # commadns for executing, first variable is name of column and capital words are commands, (data is variable)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS data (
+            Number INTEGER PRIMARY KEY AUTOINCREMENT,
+            Host TEXT,
+            Status INTEGER,
+            Latency REAL,
+            Timestamp TEXT)
+    """)
+
+    # saving all changes and closing .db file
+    database.commit()
+    database.close()
+
+def save_data(host, status, latency):
+
+    # creating connection with database (if doesn't exist, it will create new)
+    database = sqlite3.connect(data_log_file)
+    
+    # creating SQL cursor, cmds are executed through this cursor
+    cursor = database.cursor()
+
+    # Datetime customization for the right time format
+    date_now = datetime.datetime.now()
+    date_now = date_now.strftime("%H:%M:%S %m-%d-%Y")
+    
+    # Rounding latency time to 2 decimal places and adding ms
+    latency = f"{latency:.2f} ms"
+
+    # writing collected data to variable (data) which contains (Host, Status, Latency, Timestamp), "?" is position where are data written down
+    cursor.execute("INSERT INTO data (Host, Status, Latency, Timestamp) VALUES (?, ?, ?, ?)",
+                   (host, status, latency, date_now))
+
+    # saving all changes and closing .db file
+    database.commit()
+    database.close()
+
+# Calling functions and printing results
+if __name__ == '__main__':
+    data_db_init()
+
+    for host in hosts:
+        latency = pinging_hosts_latency(host)
+
+        # sqlite3 database status check
+        if latency:
+            status = "✅"
+        else: 
+            status = "❌"
+
+        save_data(host, status, latency)
+
+        # console status check
+        if pinging_hosts_latency(host):
+            print(f"✅ Host '{host}' is available, latency {latency:.2f} ms")
+        else:
+            print(f"❌ Host '{host}' is unavailable")
